2d735a4da2dea6fd91545dbc7af929b75eb892fe
owner <email>
2024-01-19 13:21:20 +0100

    commit 3

--- src/main/java/com/example/Projet.java
+++ src/main/java/com/example/Projet.java
@@ -18,4 +18,32 @@ public class Projet {
     public void ajouterTache(String titre, String desc, int duree){
         this.gestionTaches.ajouterTache(titre, desc, duree);
     }
+
+    public void supprimerTache(String titre){
+
+        Tache tacheASupprimer = null;
+
+        for(Tache tache : taches){
+            if(tache.titre.equals(titre)){
+                tacheASupprimer = tache;
+            }
+        }
+
+        if(tacheASupprimer != null){
+            taches.remove(tacheASupprimer);
+        }
+    }
+
+    public int calculerDureeTotale(){
+        
+        int counter = 0;
+        
+        for(Tache tache : taches){
+            if(tache.duree >= 0){
+                counter += tache.duree;
+            }
+        }
+
+        return counter;
+    }
 }
---
+++ src/test/java/ProjetTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import com.example.*;
+
+public class ProjetTest {
+    
+    private Projet projet;
+
+    @BeforeEach
+    void setUp(){
+        projet = new Projet("Projet de test");
+    }
+
+    @Test
+    void ajouterTacheTest(){
+        projet.ajouterTache("Test 1", "desc 1", 4);
+        assertEquals(1, projet.taches.size());
+    }
+
+    @Test
+    void supprimerTacheTest(){
+        projet.ajouterTache("Test 1", "desc 1", 4);
+        projet.supprimerTache("Test 1");
+        assertEquals(0, projet.taches.size());
+    }
+
+    @Test
+    void calculerDureeTotaleTest(){
+        projet.ajouterTache("Test 1", "desc 1", 5);
+        projet.ajouterTache("Test 2", "desc 2", 6);
+        assertEquals(11, projet.calculerDureeTotale());
+        projet.supprimerTache("Test 1");
+        projet.supprimerTache("Test 2");
+        assertEquals(0, projet.calculerDureeTotale());
+    }
+}
